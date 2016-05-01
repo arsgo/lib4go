5c09027ea606d29e8136d542141afe1ffa08248d
lib4go@163.com <lib4go@163.com>
2016-05-01 14:04:02 +0800

    'add'

--- mq/mqBinder.go
+++ mq/mqBinder.go
@@ -1,7 +1,6 @@
 package mq
 
 import (
-	"github.com/colinyl/ars/scriptservice"
 	lp "github.com/colinyl/lib4go/lua"
 	"github.com/colinyl/stomp"
 	"github.com/yuin/gopher-lua"
@@ -19,13 +18,13 @@ func NewMQBinder(c ConfigHandler, pool *lp.LuaPool) *MQBinder {
 	return &MQBinder{handler: c, pool: pool}
 }
 func (c *MQBinder) BindMQService(L *lua.LState) {
-	scriptservice.Bind(L, &scriptservice.ScriptBindClass{ClassName: "mq",
+	lp.Bind(L, &lp.ScriptBindClass{ClassName: "mq",
 		ConstructorName: "new",
 		ConstructorFunc: func(L *lua.LState) interface{} {
 			config, _ := c.handler.GetMQConfig(L.CheckString(1))
 			s, _ := NewMQService(config)
 			return s
-		}, ObjectMethods: map[string]scriptservice.ScriptBindFunc{
+		}, ObjectMethods: map[string]lp.ScriptBindFunc{
 			"close": func(L *lua.LState) (result []string) {
 				if L.GetTop() != 1 {
 					result = append(result, "input args error")
