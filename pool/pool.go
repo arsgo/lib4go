be0713aee44b2105b4ab05ed39e50693a6bf633a
colinyl <lib4net@126.com>
2016-05-02 21:00:12 +0800

    update

--- pool/pool.go
+++ pool/pool.go
@@ -20,65 +20,64 @@ func New() *ObjectPool {
 }
 
 //Register 注册指定的对象组
-func (p *ObjectPool) Register(groupName string, factory ObjectFactory, size int) int {
+func (p *ObjectPool) Register(name string, factory ObjectFactory, size int) int {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
-	if v, ok := p.pools[groupName]; ok {
+	if v, ok := p.pools[name]; ok {
 		return v.list.Len()
 	}
 
-	if v, ok := p.pools[groupName]; ok {
+	if v, ok := p.pools[name]; ok {
 		return v.list.Len()
 	}
-	p.pools[groupName] = newPoolSet(size, factory)
-	return p.pools[groupName].list.Len()
+	p.pools[name] = newPoolSet(size, factory)
+	return p.pools[name].list.Len()
 }
 
 //UnRegister 取消注册指定的对象组
-func (p *ObjectPool) UnRegister(groupName string) {
+func (p *ObjectPool) UnRegister(name string) {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
-	if v, ok := p.pools[groupName]; ok {
+	if v, ok := p.pools[name]; ok {
 		v.close()
-		delete(p.pools, groupName)
+		delete(p.pools, name)
 	}
 
 }
-func (p *ObjectPool) Exists(groupName string) bool {
+func (p *ObjectPool) Exists(name string) bool {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
-	_, ok := p.pools[groupName]
+	_, ok := p.pools[name]
 	return ok
 }
 
 //Get 从对象组中申请一个对象
-func (p *ObjectPool) Get(groupName string) (Object, error) {
+func (p *ObjectPool) Get(name string) (Object, error) {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
-	if v, ok := p.pools[groupName]; ok {
+	if v, ok := p.pools[name]; ok {
 		return v.get()
 	} else {
-		return nil, errors.New(fmt.Sprintf("not find: %s", groupName))
+		return nil, errors.New(fmt.Sprintf("not find: %s", name))
 	}
 }
 
 //Recycle 回收一个对象
-func (p *ObjectPool) Recycle(groupName string, obj Object) {
+func (p *ObjectPool) Recycle(name string, obj Object) {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
-	if v, ok := p.pools[groupName]; ok {
+	if v, ok := p.pools[name]; ok {
 		v.back(obj)
 	}
-
 }
 
 //Close 关闭一个对象组
-func (p *ObjectPool) Close(groupName string) bool {
+func (p *ObjectPool) Close(name string) bool {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
-	if ps, ok := p.pools[groupName]; ok && ps.usingCount == 0 {
+	if ps, ok := p.pools[name]; ok && ps.usingCount == 0 {
 		ps.close()
-		delete(p.pools, groupName)
+		delete(p.pools, name)
 		return true
 	}
 	return false
