8c12f606595e1786462528b1dc62d1b611ca441c
lib4go@163.com <lib4go@163.com>
2016-05-02 13:19:01 +0800

    ''

---
+++ sysinfo/sysinfo.go
@@ -0,0 +1,47 @@
+package sysinfo
+
+import (
+	"encoding/json"
+	"runtime"
+
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/mem"
+)
+
+func GetMemory() []map[string]interface{} {
+	v, _ := mem.VirtualMemory()
+	data := make(map[string]interface{})
+	buffer, _ := json.Marshal(&v)
+	json.Unmarshal(buffer, &data)
+	var result []map[string]interface{}
+	result = append(result, data)
+	return result
+}
+
+func GetCPU() []map[string]interface{} {
+	v, _ := cpu.Times(true)
+	buffer, _ := json.Marshal(&v)
+	var data []map[string]interface{}
+	json.Unmarshal(buffer, &data)
+	return data
+}
+func GetDisk() []map[string]interface{} {
+	var stats []*disk.UsageStat
+	if runtime.GOOS == "windows" {
+		v, _ := disk.Partitions(true)
+		for _, p := range v {
+			s, _ := disk.Usage(p.Device)
+			stats = append(stats, s)
+		}
+	} else {
+		s, _ := disk.Usage("/")
+		stats = append(stats, s)
+	}
+
+	buffer, _ := json.Marshal(&stats)
+	var data []map[string]interface{}
+	json.Unmarshal(buffer, &data)
+	return data
+
+}
