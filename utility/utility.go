2edfc2691d9659606241d44d5011104277db58f5
lib4go@163.com <lib4go@163.com>
2016-07-22 10:03:02 +0800

    'fix'

--- utility/utility.go
+++ utility/utility.go
@@ -67,8 +67,16 @@ func GetExcPath(p ...string) string {
 		for i := 1; i < len(p); i++ {
 			fp = strings.Trim(fp, p[i])
 		}
+
 		f, er := os.Readlink("/proc/self/exe")
-		fmt.Println("os.Readlink:", f, er)
+		fmt.Println("/proc/self/exe:", f, er)
+		
+		f, er = os.Readlink("/proc")
+		fmt.Println("proc:", f, er)
+
+		f, er = os.Readlink("/proc/self")
+		fmt.Println("proc/self:", f, er)
+
 		fmt.Println("exec path:", fp, p[0])
 		return filepath.Join(fp, strings.Trim(p[0], "."))
 	}
