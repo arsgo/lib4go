1e092054656a5864d7db8c2e9afb1f12b34aa824
arsgo <lib4net@126.com>
2016-09-06 09:44:09 +0800

    增加rsa算法

---
+++ security/rsa/rsa.go
@@ -0,0 +1,78 @@
+package rsa
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"errors"
+	"io"
+)
+
+func Encrypt(origData string, publicKey string) ([]byte, error) {
+	block, _ := pem.Decode([]byte(publicKey))
+	if block == nil {
+		return nil, errors.New("public key error")
+	}
+	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	pub := pubInterface.(*rsa.PublicKey)
+	return rsa.EncryptPKCS1v15(rand.Reader, pub, []byte(origData))
+}
+
+func Decrypt(ciphertext string, privateKey string) ([]byte, error) {
+	block, _ := pem.Decode([]byte(privateKey))
+	if block == nil {
+		return nil, errors.New("private key error!")
+	}
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	return rsa.DecryptPKCS1v15(rand.Reader, priv, []byte(ciphertext))
+}
+
+//Sign 生成签名
+func Sign(ciphertext string, privateKey string) ([]byte, error) {
+	block, _ := pem.Decode([]byte(privateKey))
+	if block == nil {
+		return nil, errors.New("private key error!")
+	}
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	return rsa.SignPKCS1v15(rand.Reader, priv, crypto.SHA1, []byte(ciphertext))
+}
+
+//Verify 验签
+func Verify(src string, sign string, pubkey string) (pass bool, err error) {
+	//步骤1，加载RSA的公钥
+	block, _ := pem.Decode([]byte(pubkey))
+	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return
+	}
+	rsaPub, _ := pub.(*rsa.PublicKey)
+
+	//步骤2，计算代签名字串的SHA1哈希
+	t := sha1.New()
+	io.WriteString(t, src)
+	digest := t.Sum(nil)
+
+	//步骤3，base64 decode,必须步骤，支付宝对返回的签名做过base64 encode必须要反过来decode才能通过验证
+	data, _ := base64.StdEncoding.DecodeString(sign)
+
+	//步骤4，调用rsa包的VerifyPKCS1v15验证签名有效性
+	err = rsa.VerifyPKCS1v15(rsaPub, crypto.SHA1, digest, data)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
